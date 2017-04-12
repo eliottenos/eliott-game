52030199f57f4523116b42277824f0c7b3c59163
eliott enos <eliottenos@gmail.com>
2017-04-12 16:37:53 -0400

    game logic
    'x' and 'o' alternate and cannot click on the same box more than once
    next looking to determine winner

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -1,8 +1,20 @@
 'use strict'
 
+// const gameArray = ['', '', '', '', '', '', '', '', '']
+// const Win = [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 5], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+let turn = 'X'
+
 const clickEvent = function () {
   $('.box').on('click', function () {
-    $('<p>X</p>').appendTo(this)
+    if (turn === 'X') {
+      $(this).html('<p>X</p>')
+      $(this).off()
+      turn = 'O'
+    } else {
+      $(this).html('<p>O</p>')
+      $(this).off()
+      turn = 'X'
+    }
   })
 }
 
