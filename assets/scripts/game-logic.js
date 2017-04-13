595a17badb7ce4926ce764068edb42bf90364b1f
eliott enos <eliottenos@gmail.com>
2017-04-13 11:24:44 -0400

    game logic
    logic for when x or o wins - problem as is determining wrong win - eg xox
    added modal to declare winner
    also need to work on adding empty array after win

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -1,19 +1,47 @@
 'use strict'
 
-// const gameArray = ['', '', '', '', '', '', '', '', '']
-// const Win = [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 5], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+let gameBoard = ['', '', '', '', '', '', '', '', '']
+// const Win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 5], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 let turn = 'X'
 
 const clickEvent = function () {
   $('.box').on('click', function () {
     if (turn === 'X') {
-      $(this).html('<p>X</p>')
+      $(this).html('X')
       $(this).off()
-      turn = 'O'
+      gameBoard[$(this).data('id')] = 'X'
+      if (((gameBoard[0] === 'X') && (gameBoard[1] === 'X') && (gameBoard[2] === 'X')) ||
+        ((gameBoard[3] === 'X') && (gameBoard[4] === 'X') && (gameBoard[5] === 'X')) ||
+        ((gameBoard[6] === 'X') && (gameBoard[7] === 'X') && (gameBoard[8] === 'X')) ||
+        ((gameBoard[0] === 'X') && (gameBoard[3] === 'X') && (gameBoard[6] === 'X')) ||
+        ((gameBoard[1] === 'X') && (gameBoard[4] === 'X') && (gameBoard[7] === 'X')) ||
+        ((gameBoard[2] === 'X') && (gameBoard[5] === 'X') && (gameBoard[8] === 'X')) ||
+        ((gameBoard[0] === 'X') && (gameBoard[4] === 'X') && (gameBoard[8] === 'X')) ||
+        ((gameBoard[2] === 'X') && (gameBoard[4] === 'X') && (gameBoard[6] === 'X'))) {
+        // game won
+        $('#winModelX').modal('show')
+        gameBoard = ['', '', '', '', '', '', '', '', '']
+      } else {
+        turn = 'O'
+      }
     } else {
-      $(this).html('<p>O</p>')
+      $(this).html('O')
       $(this).off()
-      turn = 'X'
+      gameBoard[$(this).data('id')] = 'X'
+      if (((gameBoard[0] === 'O') && (gameBoard[1] === 'O') && (gameBoard[2] === 'O')) ||
+        ((gameBoard[3] === 'O') && (gameBoard[4] === 'O') && (gameBoard[5] === 'O')) ||
+        ((gameBoard[6] === 'O') && (gameBoard[7] === 'O') && (gameBoard[8] === 'O')) ||
+        ((gameBoard[0] === 'O') && (gameBoard[3] === 'O') && (gameBoard[6] === 'O')) ||
+        ((gameBoard[1] === 'O') && (gameBoard[4] === 'O') && (gameBoard[7] === 'O')) ||
+        ((gameBoard[2] === 'O') && (gameBoard[5] === 'O') && (gameBoard[8] === 'O')) ||
+        ((gameBoard[0] === 'O') && (gameBoard[4] === 'O') && (gameBoard[8] === 'O')) ||
+        ((gameBoard[2] === 'O') && (gameBoard[4] === 'O') && (gameBoard[6] === 'O'))) {
+        // game won
+        $('#winModelO').modal('show')
+        gameBoard = ['', '', '', '', '', '', '', '', '']
+      } else {
+        turn = 'X'
+      }
     }
   })
 }
